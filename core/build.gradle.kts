df0f9867ba55ad373d4de9b42a00ce4579a326e5
Ynverxe <nahubar65@gmail.com>
2025-04-12 09:24:40 -0300

    build: remove luckperms from dependencies

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -4,7 +4,6 @@ plugins {
 
 dependencies {
     api(libs.bundles.core.base)
-    compileOnly(libs.luckperms)
     api(libs.minestom.extensions)
     implementation(kotlin("stdlib", "1.5.0"))
     implementation("org.jboss.shrinkwrap.resolver:shrinkwrap-resolver-depchain:3.1.4")
