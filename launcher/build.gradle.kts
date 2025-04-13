b84aa9e541ade595a473f283b817ed70259513ff
Ynverxe <nahubar65@gmail.com>
2025-04-12 21:04:49 -0300

    feat: custom ComponentLoggerProvider! ci: override shadowJar task to replace Minestom service implementation for ComponentLoggerProvider ci: add adventure-text-ansi and configurate-json

--- launcher/build.gradle.kts
+++ launcher/build.gradle.kts
@@ -1,3 +1,7 @@
+import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
+import java.nio.file.Files
+import java.nio.file.StandardCopyOption
+
 plugins {
     id("java")
     id("com.gradleup.shadow") version "8.3.0"
@@ -5,11 +9,26 @@ plugins {
 
 dependencies {
     implementation(project(":core"))
-
     implementation(libs.tiny.log.impl)
     implementation(libs.tiny.log.slf4j)
-
+    implementation(libs.adventure.ansi)
+    implementation(libs.configurate.json)
     //implementation("org.jetbrains:annotations:24.0.0")
+}
+
+tasks.named<ShadowJar>("shadowJar") {
+    exclude("META-INF/services/net.kyori.adventure.text.logger.slf4j.ComponentLoggerProvider")
+
+    mergeServiceFiles {
+    }
+
+    val serviceFile = file("src/main/resources/META-INF/services/net.kyori.adventure.text.logger.slf4j.ComponentLoggerProvider")
+    val tmpDir = temporaryDir.resolve("META-INF/services")
+    tmpDir.mkdirs()
+    Files.copy(serviceFile.toPath(), tmpDir.resolve("net.kyori.adventure.text.logger.slf4j.ComponentLoggerProvider").toPath(), StandardCopyOption.REPLACE_EXISTING)
+    from(tmpDir) {
+        into("META-INF/services")
+    }
 }
 
 tasks.jar {
