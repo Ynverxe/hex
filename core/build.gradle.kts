6b158e3a59ac70e924d0ae15864cf28ee037d6b2
Ynverxe <nahubar65@gmail.com>
2025-04-16 13:45:09 -0300

    refactor: move logging logic from launcher to core build: move logging dependencies and config files from launcher to core

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -5,6 +5,12 @@ plugins {
 dependencies {
     api(libs.bundles.core.base)
     api(libs.minestom.extensions)
+
+    // logging
+    implementation(libs.adventure.ansi)
+    implementation(libs.tiny.log.impl)
+    implementation(libs.tiny.log.slf4j)
+
     implementation(kotlin("stdlib", "1.5.0"))
     implementation("org.jboss.shrinkwrap.resolver:shrinkwrap-resolver-depchain:3.1.4")
 }
