ea01af0d2ec0f10ed60c3b83eb7caeb890f1ca90
Ynverxe <nahubar65@gmail.com>
2025-04-15 21:55:39 -0300

    feat: create ServerDirectorySchemeCopier

--- launcher/src/main/java/com/github/ynverxe/hexserver/launcher/HexServerLauncher.java
+++ launcher/src/main/java/com/github/ynverxe/hexserver/launcher/HexServerLauncher.java
@@ -10,11 +10,15 @@ import java.nio.file.Paths;
 public class HexServerLauncher {
 
   public static final String DONT_REGISTER_DEFAULT_LISTENERS_ARGUMENT = "DontRegisterDefaultListeners";
+  public static final String IGNORE_URL_FILES_ARGUMENT = "IgnoreUrlFiles";
 
   public static void main(String[] args) throws Exception {
     String runDir = System.getProperty("hexserver.rundir", System.getProperty("user.dir"));
 
     Path runDirPath = Paths.get(runDir);
+
+    new ServerDirectorySchemeCopier(runDirPath, isArgumentPresent(IGNORE_URL_FILES_ARGUMENT, args))
+        .start();
 
     // Download extensions
     ExtensionDownloader downloader = new ExtensionDownloader(runDirPath);
