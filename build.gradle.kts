5153e569787861d9b0c871ec654fe708a4a93604
Ynverxe <nahubar65@gmail.com>
2025-04-30 15:44:56 -0300

    build: apply groupId on all subprojects

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,5 @@
 
 subprojects {
+    group = "com.github.ynverxe.hexserver"
     version = if (rootProject.extra.has("${project.name}-version")) rootProject.extra["${project.name}-version"] as String else "undefined"
 }
