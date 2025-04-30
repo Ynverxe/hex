00833bf1783b9dc88f942159346703cf129d670e
Ynverxe <nahubar65@gmail.com>
2025-04-30 16:00:16 -0300

    ci: apply hex-publishing-conventions on all subprojects after applying the java plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,8 @@
-
 subprojects {
+    plugins.withId("java") {
+        apply(plugin = "hex-publishing-conventions")
+    }
+
     group = "com.github.ynverxe.hexserver"
     version = if (rootProject.extra.has("${project.name}-version")) rootProject.extra["${project.name}-version"] as String else "undefined"
 }
