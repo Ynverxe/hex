eef16d627ce4e57db2393fdccae7356ca56cec1a
Ynverxe <nahubar65@gmail.com>
2025-04-30 21:03:45 -0300

    feat(generator): extend Jar task class to make ConfigureGeneratorSettingsTask more configurable, also stop using :launcher:generateLauncher task

--- launcher/generator/src/main/java/com/github/ynverxe/hexserver/ConfigureGeneratorSettingsTask.java
+++ launcher/generator/src/main/java/com/github/ynverxe/hexserver/ConfigureGeneratorSettingsTask.java
@@ -1,57 +1,59 @@
 package com.github.ynverxe.hexserver;
 
-import org.gradle.api.DefaultTask;
-import org.gradle.api.Project;
-import org.gradle.api.provider.Property;
-import org.gradle.api.tasks.Input;
-import org.gradle.api.tasks.Optional;
-import org.gradle.api.tasks.SourceSet;
-import org.gradle.api.tasks.SourceSetContainer;
+import org.gradle.api.Task;
+import org.gradle.api.file.DuplicatesStrategy;
+import org.gradle.api.file.FileTree;
+import org.gradle.api.initialization.IncludedBuild;
+import org.gradle.api.tasks.TaskReference;
+import org.gradle.jvm.tasks.Jar;
 
 import java.io.File;
+import java.nio.file.Path;
 
-public class ConfigureGeneratorSettingsTask extends DefaultTask {
-
-  private final Property<String> filename;
+public class ConfigureGeneratorSettingsTask extends Jar {
 
   public ConfigureGeneratorSettingsTask() {
-    Project project = getProject();
-    this.filename = project.getObjects().property(String.class);
+    TaskReference launcherShadowJarTask = getProject().getGradle()
+        .includedBuild("hex-server")
+        .task(":launcher:shadowJar");
 
-    doFirst(task -> configureSystemProperties(project));
+    dependsOn(launcherShadowJarTask);
+    Task processResourcesTask = getProject().getTasks().getByName("processResources");
+    dependsOn(processResourcesTask);
 
-    finalizedBy(":triggerGeneratorTask");
-  }
+    IncludedBuild hexServerBuild = getProject().getGradle()
+        .includedBuild("hex-server");
 
-  @Input
-  @Optional
-  public Property<String> getFilename() {
-    return filename;
-  }
+    Path launcherBuildDir = hexServerBuild.getProjectDir().toPath()
+        .resolve("launcher")
+        .resolve("build")
+        .resolve("libs");
 
-  private void configureSystemProperties(Project project) {
-    String outputDir = project.getLayout().getBuildDirectory()
-        .get()
-        .getAsFile().getAbsolutePath();
+    Path launcherJar = launcherBuildDir.resolve("launcher-all.jar");
 
-    System.setProperty("hex.generator.output.dir", outputDir);
-
-    SourceSet mainSources = ((SourceSetContainer) project.getExtensions().getByName("sourceSets"))
-        .named("main")
-        .get();
-
-    File resources = mainSources.getResources().getSrcDirs()
-        .stream().findFirst()
-        .orElse(null);
-
-    if (resources == null) {
-      return;
+    for (File file : processResourcesTask.getOutputs().getFiles()) {
+      from(file);
     }
 
-    System.setProperty("hex.generator.input.extra", resources.getAbsolutePath());
+    FileTree files = getProject().zipTree(launcherJar);
+    from(files);
 
-    if (filename.isPresent()) {
-      System.setProperty("hex.generator.output.filename", filename.get());
-    }
+    // Merge manifests
+    manifest(manifest -> files.getFiles().stream()
+        .filter(file -> file.getName().equals("MANIFEST.MF"))
+        .findFirst()
+        .ifPresent(manifest::from));
+
+    // Jar task excludes META-INF from external jars into final copies
+    metaInf(copySpec -> {
+      files.getFiles().stream()
+          .filter(file -> file.getAbsolutePath().contains("META-INF"))
+          .filter(file -> !file.getName().equals("MANIFEST.MF"))
+          .forEach(copySpec::from);
+    });
+
+    getArchiveFileName().convention(getProject().getName() + "-launcher.jar");
+
+    setDuplicatesStrategy(DuplicatesStrategy.INCLUDE);
   }
 }
