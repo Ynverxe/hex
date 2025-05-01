12ba93c8ac9e5bd2d2579e5ac0ed6882443fbb59
Ynverxe <nahubar65@gmail.com>
2025-04-30 21:03:45 -0300

    feat(generator): extend Jar task class to make ConfigureGeneratorSettingsTask more configurable, also stop using :launcher:generateLauncher task

--- launcher/generator/src/main/java/com/github/ynverxe/hexserver/LauncherGeneratorPlugin.java
+++ launcher/generator/src/main/java/com/github/ynverxe/hexserver/LauncherGeneratorPlugin.java
@@ -7,9 +7,6 @@ public class LauncherGeneratorPlugin implements Plugin<Project> {
 
   @Override
   public void apply(Project project) {
-    // We create a new task just to call :launcher:generateLauncher because
-    // we cannot use finalizedBy on tasks from included builds
-    project.getTasks().register("triggerGeneratorTask", task -> task.dependsOn(project.getGradle().includedBuild("hex-server").task(":launcher:generateLauncher")));
     project.getTasks().register("configureGeneratorSettings", ConfigureGeneratorSettingsTask.class);
   }
 }
