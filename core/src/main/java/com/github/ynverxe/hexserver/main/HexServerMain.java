f0964d5ffb4ef6f1086c94e506746243009d79fc
Ynverxe <nahubar65@gmail.com>
2025-05-06 21:48:55 -0300

    feat(core): expose application arguments

--- core/src/main/java/com/github/ynverxe/hexserver/main/HexServerMain.java
+++ core/src/main/java/com/github/ynverxe/hexserver/main/HexServerMain.java
@@ -2,10 +2,16 @@ package com.github.ynverxe.hexserver.main;
 
 import com.github.ynverxe.hexserver.HexServerInitializer;
 import com.github.ynverxe.hexserver.internal.ParentProcessChecker;
+import org.checkerframework.checker.units.qual.A;
+import org.jetbrains.annotations.ApiStatus;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.*;
+import java.util.function.Supplier;
 
 import static com.github.ynverxe.hexserver.util.ArgumentUtil.isArgumentPresent;
 
@@ -15,22 +21,54 @@ import static com.github.ynverxe.hexserver.util.ArgumentUtil.isArgumentPresent;
 public class HexServerMain {
 
   public static final String DONT_REGISTER_DEFAULT_LISTENERS_ARGUMENT = "DontRegisterDefaultListeners";
+  private static volatile boolean STARTED = false;
+  private static @Nullable List<String> ARGUMENTS;
 
   public static void main(String[] args) throws IOException, ClassNotFoundException {
-    if (System.getenv("parent-pid") != null) {
-      ParentProcessChecker.INSTANCE.start();
+    synchronized (HexServerMain.class) {
+      if (STARTED) {
+        throw new IllegalArgumentException("main(String[]) was already been called");
+      }
+
+      ARGUMENTS = Arrays.asList(args);
+      STARTED = true;
+
+      if (System.getenv("parent-pid") != null) {
+        ParentProcessChecker.INSTANCE.start();
+      }
+
+      String runDir = System.getProperty("hexserver.rundir", System.getProperty("user.dir"));
+
+      Path runDirPath = Paths.get(runDir);
+
+      HexServerInitializer initializer = new HexServerInitializer(runDirPath);
+
+      if (!isArgumentPresent(DONT_REGISTER_DEFAULT_LISTENERS_ARGUMENT, args)) {
+        initializer.registerDefaultListeners();
+      }
+
+      initializer.start();
     }
+  }
 
-    String runDir = System.getProperty("hexserver.rundir", System.getProperty("user.dir"));
+  public static boolean started() {
+    return STARTED;
+  }
 
-    Path runDirPath = Paths.get(runDir);
+  public static @NotNull Optional<List<String>> arguments() {
+    return Optional.ofNullable(ARGUMENTS)
+        .map(Collections::unmodifiableList);
+  }
 
-    HexServerInitializer initializer = new HexServerInitializer(runDirPath);
-
-    if (!isArgumentPresent(DONT_REGISTER_DEFAULT_LISTENERS_ARGUMENT, args)) {
-      initializer.registerDefaultListeners();
+  @ApiStatus.Internal
+  public static void requiresMainStarted(@NotNull Object consumer) {
+    if (!started()) {
+      throw requiresMainStartedException(consumer).get();
     }
+  }
 
-    initializer.start();
+  @ApiStatus.Internal
+  public static Supplier<IllegalStateException> requiresMainStartedException(@NotNull Object consumer) {
+    return () -> new IllegalStateException(consumer + " cannot be used before main is started");
   }
 }
