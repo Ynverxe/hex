1358538fe16c31667a1258d58d0ddf39860c8e34
Ynverxe <nahubar65@gmail.com>
2025-05-11 17:23:30 -0300

    ci(core): configure junit tests

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -14,6 +14,11 @@ dependencies {
 
     runtimeOnly(kotlin("stdlib", "1.5.0"))
     runtimeOnly("org.jboss.shrinkwrap.resolver:shrinkwrap-resolver-depchain:3.1.4")
+
+    testImplementation("org.junit.jupiter:junit-jupiter:5.7.1")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+
+    testImplementation(libs.minestom)
 }
 
 tasks.shadowJar {
@@ -22,5 +27,24 @@ tasks.shadowJar {
     manifest {
         attributes["Implementation-Title"] = "HexServer Core"
         attributes["Implementation-Version"] = project.version
+    }
+}
+
+configurations {
+    testImplementation {
+        extendsFrom(configurations.implementation.get())
+    }
+    testRuntimeOnly {
+        extendsFrom(configurations.runtimeOnly.get())
+    }
+}
+
+tasks.named<Test>("test") {
+    useJUnitPlatform()
+
+    maxHeapSize = "1G"
+
+    testLogging {
+        events("passed")
     }
 }
