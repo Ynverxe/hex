bd187a718436cf200e4b79cb95000bd0abbf3c6b
Ynverxe <nahubar65@gmail.com>
2025-05-18 13:35:57 -0300

    ci: add develocity

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -10,7 +10,19 @@ pluginManagement {
     includeBuild("launcher-plugin")
 }
 
+plugins {
+    id("com.gradle.develocity") version "3.17.6" // o la versión más reciente
+}
+
+develocity {
+    buildScan {
+        termsOfUseUrl.set("https://gradle.com/terms-of-service")
+        termsOfUseAgree.set("yes")
+    }
+}
+
 rootProject.name = "hex-server"
+
 
 include("core")
 
