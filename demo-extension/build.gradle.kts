a7fb01d4ed3643f1ada7b1e5d13c1c1d6ed77324
Ynverxe <nahubar65@gmail.com>
2025-05-18 14:46:44 -0300

    build(demo-extension): reformat build script, use SourceSet#resources instead of SourceSetOutput#resourcesDir

--- demo-extension/build.gradle.kts
+++ demo-extension/build.gradle.kts
@@ -7,32 +7,19 @@ repositories {
 }
 
 sourceSets {
-    create("dependency") {
-        java.srcDirs("src/dependency/java")
-        resources.srcDirs("src/dependency/resources")
-    }
+    create("dependency")
 }
 
 dependencies {
     compileOnly(project(":core"))
-    compileOnly(sourceSets["dependency"].output)
+    compileOnly(sourceSets["dependency"].output.classesDirs)
     add("dependencyCompileOnly", project(":core"))
 }
 
-tasks.replace("processDependencyResources", ProcessResources::class.java).apply {
-    enabled = false
-}
-
-tasks.create<ProcessResources>("dependencyResources").apply {
-    group = "build"
-}
-
 tasks.create<Jar>("makeDependencyJar") {
-    dependsOn("dependencyResources")
-
     group = "build"
     archiveClassifier.set("dependency")
 
     from(sourceSets["dependency"].output.classesDirs)
-    from(sourceSets["dependency"].output.resourcesDir)
+    from(sourceSets["dependency"].resources)
 }
