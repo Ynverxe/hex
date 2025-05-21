f3940fd2f79610132f3122ec92c230627be4be28
Ynverxe <nahubar65@gmail.com>
2025-05-21 13:11:14 -0300

    ci: add module 'lab' for testing purposes

---
+++ lab/src/main/java/com/github/ynverxe/hexserver/lab/extension/LabExtension.java
@@ -0,0 +1,23 @@
+package com.github.ynverxe.hexserver.lab.extension;
+
+import com.github.ynverxe.hexserver.extension.HexExtension;
+import com.github.ynverxe.hexserver.world.HexWorld;
+import net.minestom.server.instance.block.Block;
+import net.minestom.server.world.DimensionType;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.UUID;
+
+public class LabExtension extends HexExtension {
+  public LabExtension(@NotNull Object context) {
+    super(context);
+  }
+
+  @Override
+  protected void enable() throws Exception {
+    HexWorld world = new HexWorld(UUID.randomUUID(), DimensionType.OVERWORLD, "world");
+    worldManager().register(world);
+
+    world.setGenerator(unit -> unit.modifier().fillHeight(0, 40, Block.GRASS_BLOCK));
+  }
+}
